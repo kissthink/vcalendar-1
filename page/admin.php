1874a23ece748384bdeb007444717a7ed681bb1e
Steffen Förster <github96758@4foerster.ch>
2012-09-04 22:44:16 +0200

    master detail for property

--- page/admin.php
+++ page/admin.php
@@ -12,10 +12,30 @@ class page_admin extends Page {
 		
 		// Tab Properties
 		$tab=$tabs->addTab('Properties');
-		$users=$tab->add('Model_User');
-		$userList=$users->getRows(array('first_name', 'last_name'));
 		$form=$tab->add('Form');
-		$field=$form->addField('dropdown','User')->setModel('User');
+		$field=$form->addField('dropdown', 'user_id', 'User');
+		$field->setModel('User');
+		$field->setEmptyText('Select ...')
+			  ->js('change', $form->js()->submit());
+		
+		
+		$propModel=$this->add('Model_Property');
+        
+		if($_GET["user_id"]){
+            $propModel->setMasterField('user_id', $_GET['user_id']);
+			$this->api->stickyGET('user_id');
+        }
+		else {
+			$propModel->setMasterField('user_id', -1);
+		}
+		
+        $propCrud=$tab->add('CRUD');
+		
+        $propCrud->setModel($propModel);
+        if($form->isSubmitted()){
+             $propCrud->js()->reload(array('user_id' => $form->get('user_id')))->execute();
+        }
+		
 		
         //$tab->add('CRUD')->setModel('User');
 		
